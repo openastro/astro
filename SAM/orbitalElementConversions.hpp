bd6c1d9efd9300f93f0f41300134d16f9107a976
Kartik Kumar <me@kartikkumar.com>
2014-09-18 22:09:13 +0200

    Add function prototype for Cartesian-to-Keplerian elements conversion.

--- SAM/orbitalElementConversions.hpp
+++ SAM/orbitalElementConversions.hpp
@@ -3,7 +3,15 @@
  * All rights reserved.
  */
 
- namespace sam
- {
+namespace sam
+{
 
- } // namespace sam
+template< typename Real, typename Vector6, typename Vector3 >
+Vector6 convertCartesianToKeplerianElements(
+    const Vector6& cartesianElements, const Real centralBodyGravitationalParameter,
+    const Real tolerance = 10.0 * std::numeric_limits< Real >::epsilon( ) )
+{
+    
+}
+
+} // namespace sam
