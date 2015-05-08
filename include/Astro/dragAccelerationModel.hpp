b2e9c32a44892f0550e65ccbd6c183ef3e7eb3ca
nataliaero <natorgom@gmail.com>
2015-05-08 13:42:20 +0100

    Drag Acceleration Model and Tests

--- include/Astro/dragAccelerationModel.hpp
+++ include/Astro/dragAccelerationModel.hpp
@@ -4,6 +4,9 @@
  * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
  */
 
+#ifndef ASTRO_DRAG_ACCELERATION_MODEL_HPP
+#define ASTRO_DRAG_ACCELERATION_MODEL_HPP
+
 #include <SML/linearAlgebra.hpp>
 
 namespace astro
@@ -14,51 +17,53 @@ namespace astro
  * Computes drag acceleration using a cannonball model. The model for the acceleration is given by
  *
  * \f[
- *      a_{drag} = \frac{1}{2} \frac{C_{d}}{m} \rho V S \vec{V}
+ *      a_{drag} = \frac{1}{2} \frac{C_{d}}{m} \rho S V \vec{V}
  * \f]
  * 
  * where \f$a_{drag}\f$ is the drag acceleration, \f$C_{d}\f$ is the drag coefficient, \f$m\f$ is
- * the mass, \f$\rho\f$ is the atmospheric density, \$f\vec{V}$f is the relative velocity with 
- * respect to the body-fixed frame  and \f$S\f$ is the drag area, that is, the projected area
- * of the object perpendicular to \$f\vec{V}$f.
+ * the mass, \f$\rho\f$ is the atmospheric density, \f$\vec{V}\f$ is the relative velocity with 
+ * respect to the body-fixed frame and \f$S\f$ is the drag area, that is, the projected area
+ * of the object perpendicular to \f$\vec{V}\f$.
  *
- * @param  dragCoefficient                     Drag coefficient        [adim]
- * @param  atmosphericDensity                  Atmospheric Density     [kg * m^-3]
- * @param  velocity                            Velocity vector 3x3     [m s^-1]
- * @param  dragArea                            Drag area               [m^2]
- * @param  mass                                Mass                    [kg]
- * @return                                        
+ * @param[in]  dragCoefficient      Drag coefficient               [adim]
+ * @param[in]  atmosphericDensity   Atmospheric density            [kg m^-3]
+ * @param[in]  velocity             Velocity vector (3x1)          [m s^-1]
+ * @param[in]  dragArea             Drag area                      [m^2]
+ * @param[in]  mass                 Mass                           [kg]
+ * @return                          Drag acceleration vector (3x1) [m s^-2]                                                      
  */
- template< typename Real, typename Vector3 >
- Vector3 computeDragAcceleration( Real    dragCoefficient, 
-                                  Real    atmosphericDensity, 
-                                  Vector3 velocity,
-                                  Real    dragArea,
-                                  Real    mass ); 
+template< typename Real, typename Vector3 >
+Vector3 computeDragAcceleration( const Real     dragCoefficient, 
+                                 const Real     atmosphericDensity, 
+                                 const Vector3& velocity,
+                                 const Real     dragArea,
+                                 const Real     mass ); 
 
+//! Compute drag acceleration on a cannonball.
+template< typename Real, typename Vector3 >
+Vector3 computeDragAcceleration( const Real     dragCoefficient, 
+                                 const Real     atmosphericDensity, 
+                                 const Vector3& velocity,
+                                 const Real     dragArea,
+                                 const Real     mass )
+{
+    Vector3 dragAcceleration = velocity;
 
- //! Compute drag acceleration on a cannonball.
- template< typename Real, typename Vector3 >
- Vector3 computeDragAcceleration( Real    dragCoefficient, 
-                                  Real    atmosphericDensity, 
-                                  Vector3 velocity,
-                                  Real    dragArea,
-                                  Real    mass )
- {
-    Vector3 dragAcceleration( 3 );
+    // Compute the squared norm of the velocity.
+    const Real normVelocity = sml::norm< Real >( velocity );
 
-    // Compute the squared norm of the velocity
-    Real normVelocity = sml::norm< Real >( velocity );
+    // Compute a premultiplier so that it does not have to be written several times.
+    const Real preMultiplier = 0.5 * dragCoefficient * atmosphericDensity 
+                               * dragArea * normVelocity / mass;
 
-    // Compute a premultiplier so that it does not have to be written several times
-    Real preMultiplier = 0.5 * dragCoefficient * atmosphericDensity * dragArea * normVelocity/mass;
-
-    // Compute the drag acceleration vector
+    // Compute the drag acceleration vector.
     dragAcceleration[ 0 ] = preMultiplier * velocity[ 0 ];
     dragAcceleration[ 1 ] = preMultiplier * velocity[ 1 ];
     dragAcceleration[ 2 ] = preMultiplier * velocity[ 2 ];
 
     return dragAcceleration;
- }
+}
 
 } // namespace astro
+
+#endif // ASTRO_DRAG_ACCELERATION_MODEL_HPP
--- test/testDragAccelerationModel.cpp
+++ test/testDragAccelerationModel.cpp
@@ -4,22 +4,22 @@
  * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
  */
 
+#include <cmath>
 #include <vector>
 
 #include <catch.hpp>
 
-#include <Astro/dragAccelerationModel.hpp>
+#include "Astro/dragAccelerationModel.hpp"
 
 namespace astro
 {
-
 namespace tests
 {
 
 typedef double Real;
 typedef std::vector< Real > Vector;
 
-TEST_CASE( "Obtain drag acceleration", "[obtain-drag-acceleration]" )
+TEST_CASE( "Obtain drag acceleration: test 1", "[drag, acceleration, models]" )
 {
     // Set expected drag acceleration vector [m/s].
     Vector expectedDragAcceleration( 3 );
@@ -28,12 +28,12 @@ TEST_CASE( "Obtain drag acceleration", "[obtain-drag-acceleration]" )
     expectedDragAcceleration[ 2 ] = 0.000154000157143e-4;
 
     // Set epsilon = error between expected value and computed value.
-    const Real epsilon = 1e-10;
+    const Real epsilon = 1.0e-10;
 
     // Set drag coefficient.
     const Real dragCoefficient = 2.2;
 
-    // Set atmospheric density [kg/m3].
+    // Set atmospheric density [kg/m^3].
     const Real atmosphericDensity = 2.0e-11;
 
     // Velocity vector in [m/s].
@@ -42,18 +42,62 @@ TEST_CASE( "Obtain drag acceleration", "[obtain-drag-acceleration]" )
     velocity[ 1 ] = 0.0;
     velocity[ 2 ] = 10.0;
 
-    // Set drag area [m^2]
+    // Set drag area [m^2].
     const Real dragArea = 5.0;
 
-    // Set mass [kg]
+    // Set mass [kg].
     const Real mass = 500.0;
 
      //! Compute drag acceleration.
     const Vector dragAcceleration = computeDragAcceleration( dragCoefficient, 
-                                                              atmosphericDensity, 
-                                                              velocity,
-                                                              dragArea,
-                                                              mass ); 
+                                                             atmosphericDensity, 
+                                                             velocity,
+                                                             dragArea,
+                                                             mass ); 
+
+    // Check if computed mean motion matches expected value.
+    REQUIRE( std::fabs( dragAcceleration[ 0 ] - expectedDragAcceleration[ 0 ] ) <= epsilon );
+    REQUIRE( std::fabs( dragAcceleration[ 1 ] - expectedDragAcceleration[ 1 ] ) <= epsilon );
+    REQUIRE( std::fabs( dragAcceleration[ 2 ] - expectedDragAcceleration[ 2 ] ) <= epsilon );
+}
+
+TEST_CASE( "Obtain drag acceleration: test 2", "[obtain-drag-acceleration-2]" )
+{
+    // Reference: http://tudat.tudelft.nl/
+
+    // Set expected drag acceleration vector [m/s].
+    Vector expectedDragAcceleration( 3 );
+    expectedDragAcceleration[ 0 ] = 0.0;
+    expectedDragAcceleration[ 1 ] = 0.0;
+    expectedDragAcceleration[ 2 ] = 267.4212;
+
+    // Set epsilon = error between expected value and computed value.
+    const Real epsilon = 1.0e-10;
+
+    // Set drag coefficient.
+    const Real dragCoefficient = 1.1;
+
+    // Set atmospheric density [kg/m3].
+    const Real atmosphericDensity = 3.5e-5;
+
+    // Velocity vector in [m/s].
+    Vector velocity( 3 );
+    velocity[ 0 ] = 0.0;
+    velocity[ 1 ] = 0.0;
+    velocity[ 2 ] = 3491.0;
+
+    // Set drag area [m^2]
+    const Real dragArea = 2.2;
+
+    // Set mass [kg]
+    const Real mass = 1.93;
+
+     //! Compute drag acceleration.
+    const Vector dragAcceleration = computeDragAcceleration( dragCoefficient, 
+                                                             atmosphericDensity, 
+                                                             velocity,
+                                                             dragArea,
+                                                             mass ); 
 
     // Check if computed mean motion matches expected value.
     REQUIRE( abs(dragAcceleration[ 0 ] - expectedDragAcceleration[ 0 ]) <= epsilon );
