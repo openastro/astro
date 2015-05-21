406c67834e8dcc7852709c6e95873d825a3ee6a4
Marko Jankovic <marko.jankovic@dfki.de>
2015-05-21 14:39:16 +0100

    Add central body acceleration model with some tests.
    Fix a formatting issue in solarRadiationPressureAccelerationModel.hpp.

--- include/Astro/solarRadiationPressureAccelerationModel.hpp
+++ include/Astro/solarRadiationPressureAccelerationModel.hpp
@@ -68,7 +68,7 @@ Vector3 computeSolarRadiationPressureAcceleration( const Real     radiationPress
     radiationPressureAcceleration[ 2 ] = radiationPressureMagnitude * vectorToSource[ 2 ];
 
     return radiationPressureAcceleration;
-};
+}
 
 } // namespace astro
 
